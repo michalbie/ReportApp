d6609d04ec11bb0c51f8157bd1ade6be2f9f68d9
Michal Biernacki <userofficeit@gmail.com>
2020-02-18 21:50:02 +0100

    Create the first version of reporting application
    Created the first v ersion of reporting application

---
+++ backend.py
@@ -0,0 +1,105 @@
+import json
+import mysql.connector
+from datetime import date, datetime
+
+class UserData:
+    def __init__(self):
+        self.data = '{}'
+        self.load_data()
+
+    def load_data(self):
+        with open('data.json', 'r') as f:
+            self.data = json.load(f)
+
+    def update_data(self, new_name, new_surname):
+        self.data['logged_once'] = 'Yes'
+        self.data['name'] = new_name
+        self.data['surname'] = new_surname
+
+        self.write_data(self.data)
+
+    def write_data(self, d):
+        with open('data.json', 'w') as outfile:
+            json.dump(d, outfile)
+
+
+
+class Database:
+
+    class DatabaseData:
+        def __init__(self):
+            self.data = '{}'
+            self.load_data()
+            self.db_name = self.data['db_name']
+            self.username = self.data['username']
+            self.password = self.data['password']
+            self.host = self.data['host']
+            self.port = int(self.data['port'])
+            self.today = date.today()
+            self.today_format = self.today.strftime("%Y-%m-%d")
+
+        def load_data(self):
+            with open('db_data.json', 'r') as f:
+                self.data = json.load(f)
+
+    def __init__(self):
+        self.db_connector = 0
+        self.db_data = self.DatabaseData()
+        self.connect()
+        self.query = ""
+
+    def connect(self):
+        self.db_connector = mysql.connector.connect(user=self.db_data.username,
+                                                    password=self.db_data.password,
+                                                    host=self.db_data.host,
+                                                    database=self.db_data.db_name,
+                                                    port=self.db_data.port)
+        self.cursor = self.db_connector.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.db_connector.close()
+
+    def send_raport(self, u_name, u_surname, u_raport):
+        self.query = "INSERT INTO Raports (name, surname, date, raport) VALUES(%s, %s, %s, %s)"
+        values = (u_name, u_surname, str(self.db_data.today_format), u_raport)
+        self.cursor.execute(self.query, values)
+        self.db_connector.commit()
+
+    def generate_raport(self):
+        generated_raport = ""
+        self.query = "SELECT name, surname, raport FROM Raports WHERE date = " + "'" + str(self.db_data.today_format) + "'"
+        self.cursor.execute(self.query)
+        for (name, surname, raport) in self.cursor:
+            generated_raport += "{} {} \n {} \n\n".format(name, surname, raport)
+        return generated_raport
+
+    def check_if_pushed(self):
+        self.query = "SELECT date FROM History WHERE date = " + "'" + str(self.db_data.today_format) + "'"
+        self.cursor.execute(self.query)
+        is_already_pushed = 0
+        for date in self.cursor:
+            is_already_pushed = 1
+        return is_already_pushed
+
+    def push_raport(self):
+        self.query = "INSERT INTO History (date) VALUE (" + "'" + str(self.db_data.today_format) + "'" + ")"
+        self.cursor.execute(self.query)
+        self.db_connector.commit()
+
+    def load_dates_to_list(self):
+        self.query = "SELECT DISTINCT date FROM Raports"
+        self.cursor.execute(self.query)
+        list = []
+        iter = 0
+        for date in self.cursor:
+            list.append(date[iter].strftime("%Y-%m-%d"))
+        return list
+
+    def show_history(self, selected_date):
+        generated_raport = ""
+        self.query = "SELECT name, surname, raport FROM Raports WHERE date = " + "'" + str(selected_date) + "'"
+        self.cursor.execute(self.query)
+        for (name, surname, raport) in self.cursor:
+            generated_raport += "{} {} \n {} \n\n".format(name, surname, raport)
+        return generated_raport
---
+++ main.py
@@ -0,0 +1,100 @@
+from ear_ui import *
+from backend import *
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QObject, pyqtSignal
+
+
+class MainWindow(QMainWindow, Ui_MainWindow, QObject):
+    was_logged = pyqtSignal()
+
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent=parent)
+        self.setupUi(self)
+        self.user = UserData()
+        self.database = Database()
+        self.connect_signals()
+        self.check_if_logged()
+        self.load_list_to_comboBox()
+        self.show()
+
+    def connect_signals(self):
+        self.enter_button.pressed.connect(self.exit_login_page)
+        self.enter_button.pressed.connect(self.set_user_data)
+        self.was_logged.connect(self.exit_login_page)
+
+        self.raport_button.pressed.connect(self.set_raport_page)
+        self.generate_button.pressed.connect(self.set_generate_page)
+        self.history_button.pressed.connect(self.set_history_page)
+
+        self.submitRaport_button.pressed.connect(self.on_submit_raport)
+        self.generateRaport_button.pressed.connect(self.on_generate_raport)
+        self.push_button.pressed.connect(self.on_push_raport)
+
+        self.history_comboBox.currentIndexChanged.connect(self.on_show_history)
+
+    def exit_login_page(self):
+        self.outer_stackedWidget.setCurrentIndex(1)
+
+    def set_user_data(self):
+        new_name = self.name_lineEdit.text()
+        new_surname = self.surname_lineEdit.text()
+        self.user.update_data(new_name, new_surname)
+
+    def set_raport_page(self):
+        self.inner_stackedWidget.setCurrentIndex(0)
+
+    def set_generate_page(self):
+        self.inner_stackedWidget.setCurrentIndex(1)
+
+    def set_history_page(self):
+        self.inner_stackedWidget.setCurrentIndex(2)
+
+    def check_if_logged(self):
+        if self.user.data['logged_once'] == 'Yes':
+            self.was_logged.emit()
+
+    def load_list_to_comboBox(self):
+        list = self.database.load_dates_to_list()
+        for date in list:
+            self.history_comboBox.addItem(date)
+
+    def on_submit_raport(self):
+        if self.user.data['last_raport'] < self.database.db_data.today_format:
+            self.database.send_raport(self.user.data['name'], self.user.data['surname'], self.raport_textEdit.toPlainText())
+            self.raport_textEdit.clear()
+            self.user.data['last_raport'] = self.database.db_data.today_format
+            with open('data.json', 'w') as outfile:
+                json.dump(self.user.data, outfile)
+        else:
+            QtWidgets.QMessageBox.warning(self, "Error", "You have sent a raport today.")
+
+    def on_generate_raport(self):
+        if self.ownerPassword_lineEdit.text() == self.user.data['owner_passwd']:
+            raport = self.database.generate_raport()
+            self.generatedRaport_textEdit.setReadOnly(False)
+            self.generatedRaport_textEdit.setText(raport)
+            self.generatedRaport_textEdit.setReadOnly(True)
+        else:
+            QtWidgets.QMessageBox.warning(self, "Error", "Wrong password. If you are not a boss, you can't generate daily raport.")
+
+    def on_push_raport(self):
+        if self.database.check_if_pushed() == 0 and self.ownerPassword_lineEdit.text() == self.user.data['owner_passwd']:
+            self.database.push_raport()
+            QtWidgets.QMessageBox.information(self, "Pushed", "Today's report has been registered.")
+        else:
+            QtWidgets.QMessageBox.warning(self, "Error", "Wrong password or you provided wrong password.")
+
+    def on_show_history(self):
+        history_record = self.database.show_history(self.history_comboBox.currentText())
+        self.history_textEdit.setReadOnly(False)
+        self.history_textEdit.setText(history_record)
+        self.history_textEdit.setReadOnly(True)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
+
+#LUhC!I3eh)FASEJ( password for webpage
